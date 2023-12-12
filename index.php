88ffdc9cd8b3780c6ab8ff3df483859f73a71c31
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2023-12-12 23:49:34 +0100

    Making more changes

--- index.php
+++ index.php
@@ -9,15 +9,43 @@ shuffle($flashcards);
 
 $score = 0;
 
-// Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $selectedAnswer = $_POST["answer"];
+
+    $correctAnswer = $_SESSION["flashcards"][$_SESSION["currentQuestion"]];
+
+    if ($selectedAnswer === $correctAnswer) {
+        $score++;
+    }
+
+    $_SESSION["currentQuestion"]++;
+
+    if ($_SESSION["currentQuestion"] >= count($_SESSION["flashcards"])) {
+        echo "<h2>Your final score is $score out of " . count($_SESSION["flashcards"]) . ".</h2>";
+        session_destroy();
+    }
 }
 
-$correctAnswer = $_SESSION["flashcards"][$_SESSION["currentQuestion"]];
+?>
 
-if ($selectedAnswer === $correctAnswer) {
-    $score++;
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Flashcard Game</title>
+</head>
+<body>
+
+
+<?php
+session_start();
+
+if (!isset($_SESSION["flashcards"])) {
+    $_SESSION["flashcards"] = $flashcards;
+    shuffle($_SESSION["flashcards"]);
+    $_SESSION["currentQuestion"] = 0;
+    $score = 0;
 }
-
 ?>
