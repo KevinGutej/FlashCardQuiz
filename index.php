ec48a9a50cb7d9f962d886bf437733adf6ffc92a
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2023-12-14 23:50:44 +0100

    Final commit

--- index.php
+++ index.php
@@ -31,12 +31,13 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Flashcard Game</title>
 </head>
-<body>
+
 
 
 <?php
@@ -49,3 +50,16 @@ if (!isset($_SESSION["flashcards"])) {
     $score = 0;
 }
 ?>
+
+<?php if ($_SESSION["currentQuestion"] < count($_SESSION["flashcards"])) : ?>
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+        <h2><?php echo key($_SESSION["flashcards"]); ?></h2>
+        <label for="answer">Type your answer:</label>
+        <input type="text" name="answer" required>
+        <button type="submit">Submit your Answer</button>
+    </form>
+<?php endif; ?>
+
+<body>
+
+</html>
